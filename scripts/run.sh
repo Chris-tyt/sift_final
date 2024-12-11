4e4c159dead9b90109ca0fd29e32ca8def638de1
Chris <chris@outlook.com>
2024-12-11 14:04:51 -0800

    delete img

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,9 +4,9 @@ cmake ..
 make
 cd ..
 # mode can be either test or plot; other values are invalid
-mode=plot
+mode=test
 # image_num is the number of images to process; 0 means all images
-image_num=2
+image_num=3
 
 # Run the script bin/match_festures, passing mode and image_num as parameters
 if [ "$image_num" -ne 0 ]; then
